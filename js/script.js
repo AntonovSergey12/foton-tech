4a636666632a1f25b9c07bae09c56920aaf3cbaa
AntonovSergey12 <sergeyantonov2011@gmail.com>
2022-01-22 13:01:12 +0200

    added css popup

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const buttonCall = document.querySelector('.banner-button')
 const popupSection = document.querySelector('.popup-container')
 const closePopUp = document.querySelector('.popup-close')
 
+const buttonOddzvon = document.querySelector('.footer-button')
+const popupContent = document.querySelector('.popup-container-oddzwon')
+const closePopUpOddzwon = document.querySelector('.popup-close-oddzwon')
 
 buttonCall.addEventListener('click', function () {
     popupSection.classList.add('popup-container-show')
@@ -9,4 +12,11 @@ buttonCall.addEventListener('click', function () {
 
 closePopUp.addEventListener('click', function () {
     popupSection.classList.remove('popup-container-show')
+})
+
+buttonOddzvon.addEventListener('click', function () {
+    popupContent.classList.add('popup-container-oddzwon-show')    
+})
+closePopUpOddzwon.addEventListener('click', function () {
+    popupContent.classList.remove('popup-container-oddzwon-show')
 })
